5aa40cc3740cbeb4450768f80b546105dd367415
moeinkeyvanipur <moein.kpr@gmail.com>
2022-10-19 14:04:34 +0330

    changing year and day in UI

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/aref/Library/Flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/aref/Projects/Flutter/jalali_calendar/example"
+export "FLUTTER_ROOT=C:\src\flutter_windows_2.10.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\flutter_project\jalali_calendar_new\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/src/datepicker.dart
+++ lib/src/datepicker.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'persian_date.dart';
+
 typedef DateChangedCallback(int? year, int? month, int? date);
 
 const String _kDateFormatDefault = 'yyyy-mm-dd';
@@ -148,8 +149,7 @@ class _DatePickerRoute<T> extends PopupRoute<T> {
 
 class _DatePickerComponent extends StatefulWidget {
   _DatePickerComponent(
-      {
-      required this.route,
+      {required this.route,
       this.minYear: _kDefaultMinYear,
       this.maxYear: _kDefaultMaxYear,
       this.initialYear: -1,
@@ -428,16 +428,16 @@ class _DatePickerState extends State<_DatePickerComponent> {
     String monthAppend = "";
     String dayAppend = "";
 
-    List<Widget> pickers =  [];
+    List<Widget> pickers = [];
     List<String> formatSplit = widget.dateFormat!.split('-');
     for (int i = 0; i < formatSplit.length; i++) {
       var format = formatSplit[i];
-      if (format.contains("yy")) {
-        pickers.add(_renderYearsPickerComponent(yearAppend));
+      if (format.contains("dd")) {
+        pickers.add(_renderDaysPickerComponent(dayAppend));
       } else if (format.contains("mm")) {
         pickers.add(_renderMonthsPickerComponent(monthAppend, format: format));
-      } else if (format.contains("dd")) {
-        pickers.add(_renderDaysPickerComponent(dayAppend));
+      } else if (format.contains("yy")) {
+        pickers.add(_renderYearsPickerComponent(yearAppend));
       }
     }
 
